ea74306ed9268396e9a73f4894b7b1c6764d370b
Fernanda <fernanda_dehays@hotmail.com>
2020-01-19 22:41:19 +0100

    import provider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import Services from '../components/Services'
+import FeaturedRooms from '../components/FeaturedRooms'
 
 import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <>
+    <React.Fragment>
     <Hero>
       <Banner 
         title="luxurious rooms"
@@ -20,6 +21,7 @@ export default function Home() {
 
     </Hero>
     <Services />
-   </>
+    <FeaturedRooms />
+   </React.Fragment>
   )
 }
