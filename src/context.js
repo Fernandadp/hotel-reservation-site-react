674e20e58ab080dad52ff38f57d3a0aaa753dcc0
Fernanda Dehays <fernanda.dehays@fernandas-mbp.localdomain>
2020-04-18 00:01:31 +0200

    modify context, add new props, set rooms page to display all rooms available

--- src/context.js
+++ src/context.js
@@ -9,7 +9,16 @@ class RoomProvider extends Component {
     rooms: [],
     sortedRooms: [],
     featuredRooms: [],
-    loading: true
+    loading: true,
+    type: 'all',
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false
   };
 
   //getData
@@ -18,12 +27,18 @@ class RoomProvider extends Component {
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter(room => room.featured === true);
 
+    let maxPrice = Math.max(...rooms.map(item => item.price));
+    let maxSize = Math.max(...rooms.map(item => item.size));
+
     this.setState({
       rooms, 
       featuredRooms, 
       sortedRooms: rooms, 
-      loading: false
-    })
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize
+    });
   }
 
   formatData(items) {
@@ -46,12 +61,25 @@ class RoomProvider extends Component {
     return room;
   }
 
+  handleChange = event => {
+    const type = event.target.type;
+    const name = event.target.name;
+    const value = event.target.value;
+
+    console.log(type, name, value);
+  }
+
+  filterRooms = () => {
+    console.log('hello')
+  }
+
   render() {
     return (
       <RoomContext.Provider 
         value={{
           ...this.state,
-          getRoom: this.getRoom
+          getRoom: this.getRoom,
+          handleChange: this.handleChange
         }}
       >
         {this.props.children}
@@ -61,6 +89,15 @@ class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component) {
+  return function ConsumerWrapper(props) {
+    return <RoomConsumer>
+      { value => <Component { ...props } context={value} />}
+
+    </RoomConsumer>
+  }
+}
 
 
 export { RoomProvider, RoomConsumer, RoomContext }
