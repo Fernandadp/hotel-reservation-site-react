6431ef88743c925ca04cf12f7b02102b0c1ac827
Fernanda Dehays <fernanda.dehays@fernandas-mbp.localdomain>
2020-04-18 00:00:44 +0200

    add new components

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,13 +1,58 @@
 import React from 'react';
 import RoomFilter from './RoomFilter';
 import RoomList from './RoomList';
+import Loading from './Loading';
 
-export default function RoomContainer() {
-    return (
-        <div>
-            Hello from Rooms Container
-            <RoomFilter />
-            <RoomList />
-        </div>
-    )
+import { withRoomConsumer } from '../context';
+
+function RoomContainer({context}) {
+  const { loading, sortedRooms, rooms } = context;
+
+  if(loading) {
+    return<Loading />; 
+  }
+
+  return (
+    <>
+      <RoomFilter rooms={rooms} />
+      <RoomList rooms={sortedRooms} />
+    </>
+  )
+        
 }
+
+export default withRoomConsumer(RoomContainer);
+
+//second options to build this:
+
+// import React from 'react';
+// import RoomFilter from './RoomFilter';
+// import RoomList from './RoomList';
+// import Loading from './Loading';
+
+// import { RoomConsumer } from '../context';
+
+// export default function RoomContainer() {
+//   return (
+//     <RoomConsumer>
+//       {
+//         (value) => {
+//           const { loading, sortedRooms, rooms } = value;
+          
+//           if(loading) {
+//             return<Loading />; 
+//           }
+
+//           return (
+//             <div>
+//               Hello from Rooms Container
+//               <RoomFilter rooms={rooms} />
+//               <RoomList rooms={sortedRooms} />
+//             </div>
+//           )
+//         }
+//       }        
+    
+//     </RoomConsumer>
+//   )
+// }
